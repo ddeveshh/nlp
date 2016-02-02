8c96a35fa2a406ae07cb7f36a48ac0b79268b54e
SACHIN MITTAL <m.sachin@iitg.ernet.in>
2016-02-02 17:11:53 +0530

    part1

--- part1_1.py
+++ part1_1.py
@@ -15,15 +15,61 @@ bi_tokens = list(bigrams(tokens))					# getting the bigrams
 tri_tokens = list(trigrams(tokens))
 
 
+uni_fdist = nltk.FreqDist(tokens)
+
+uni_freq = 0
+
+for k,v in uni_fdist.most_common():
+	#print k,v
+	uni_freq = uni_freq + v
+x = uni_freq*(0.9)
+no1 = 0
+for k,v in uni_fdist.most_common():
+	x = x-v
+	#print k,v
+	if x > 0:
+		no1 = no1+1
 
 
-fdist = nltk.FreqDist(bi_tokens)
-t = fdist1.most_common(10)
-for k,v in t.items():
-    print k,v
+print "for 90 percentage unigrams needed ",no1
+
+#print x
+#print uni_freq
+#print len(uni_fdist)
 
 
-for k,v in fdist.most_common():
-    print k,v
+
+bi_fdist = nltk.FreqDist(bi_tokens)
+#print len(bi_fdist)
+bi_freq = 0
+for k,v in bi_fdist.most_common():
+    #print k,v
+    bi_freq = bi_freq + v
+
+x = bi_freq*(0.8)
+#print x
+no2 = 0
+for k,v in bi_fdist.most_common():
+	x = x-v
+	#print k,v
+	if x > 0:
+		no2 = no2+1
+
+print "for 80 percentage bigrams needed ",no2
+
+tri_fdist = nltk.FreqDist(tri_tokens)
+tri_freq = 0
+for k,v in tri_fdist.most_common():
+	tri_freq = tri_freq + v
+	#print k,v
+x = tri_freq*(0.7)
+no1 = 0
+for k,v in tri_fdist.most_common():
+	x = x-v
+	#print k,v
+	if x > 0:
+		no1 = no1+1
+		
+print "for 70 percentage trigrams needed ",no1
 
 
